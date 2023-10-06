6079c123a0dde626496d933e54b20780d9d3e598
rchua0526 <59790199+rchua0526@users.noreply.github.com>
2023-10-05 17:26:42 -0700

    samples

---
+++ 127/samples.sql
@@ -0,0 +1,4 @@
+-- SELECT * FROM PEOPLE;
+-- SELECT pfirstname, plastname, pstate, pcity FROM PEOPLE;
+-- SELECT pfirstname, plastname, pstate, pcity FROM people WHERE pstate = 'NY' AND pcity = 'Cincinnati';
+SELECT pfirstname, plastname, pstate, pcity FROM people WHERE pstate = 'NY' OR pcity = 'Cincinnati';
