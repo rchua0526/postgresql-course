6e11393d25cd80865d150080603e3d2b7fd98bf9
rchua0526 <59790199+rchua0526@users.noreply.github.com>
2023-10-22 17:05:52 -0700

    week 16 and 17 Ninja levels

---
+++ 202/sample.sql
@@ -0,0 +1,14 @@
+SELECT * FROM customers;
+SELECT * FROM rentals;
+SELECT * FROM movies;
+
+DELETE FROM customers
+USING rentals, movies
+WHERE customers.cid = rentals.cid
+    AND rentals.mid = movies.mid
+    AND movies.mname = 'The Lives of Others'
+RETURNING *;
+
+SELECT * FROM customers;
+SELECT * FROM rentals;
+SELECT * FROM movies;
